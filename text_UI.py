89bde316151e0faa2275576c61c7751af95074db
Aydan Eng <94135697+qydan@users.noreply.github.com>
2025-02-06 18:51:59 -0500

    Update text_UI.py

--- text_UI.py
+++ text_UI.py
@@ -1,9 +1,9 @@
 # ECOR 1042 Lab 6 - Template text UI
 # Update "" with your name (e.g., Cristina Ruiz Martin)
-__author__ = "Aydan Eng"
+__author__ = ""
 
 # Update "" with your student number (e.g., 100100100)
-__student_number__ = "101298520"
+__student_number__ = ""
 
 # Update "" with your team (e.g. T-102, use the notation provided in the example)
 __team__ = "T-097"
