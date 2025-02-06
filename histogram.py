0916dd621b759ccc0586f05267f81966cb946f1c
Aydan Eng <94135697+qydan@users.noreply.github.com>
2025-02-06 18:51:37 -0500

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -3,10 +3,10 @@
 # Remember to include docstring and type annotations for your functions
 
 # Update "" with your name (e.g., Cristina Ruiz Martin)
-__author__ = "Peter Abou Mrad"
+__author__ = ""
 
 # Update "" with your student number (e.g., 100100100)
-__student_number__ = "101297408"
+__student_number__ = ""
 
 # Update "" with your team (e.g. T-102, use the notation provided in the example)
 __team__ = "T-097"
